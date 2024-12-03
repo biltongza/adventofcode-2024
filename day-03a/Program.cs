17afc0a05e69c205ca93a82801678b9b56ecf249
Logan Dam <dam.loganza@gmail.com>
2024-12-03 12:04:07 +0100

    day-03a

---
+++ day-03a/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text.RegularExpressions;
+var file = File.ReadAllText("input.txt");
+var matches = Helpers.MulExpression().Matches(file);
+int sum = 0;
+foreach (var match in matches.Cast<Match>())
+{
+    var a = match.Groups[1].Value;
+    var b = match.Groups[2].Value;
+    sum += int.Parse(a) * int.Parse(b);
+}
+Console.WriteLine(sum);
+
+static partial class Helpers
+{
+    [GeneratedRegex("mul\\((\\d{1,3}),(\\d{1,3})\\)")]
+    public static partial Regex MulExpression();
+}
